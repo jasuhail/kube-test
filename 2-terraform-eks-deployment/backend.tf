957c43725630136bbf34fadaf705fd1a7a5c489f
Ameer Suhail <80986120+jasuhail@users.noreply.github.com>
2025-05-17 08:08:38 +0530

    Update backend.tf

--- 2-terraform-eks-deployment/backend.tf
+++ 2-terraform-eks-deployment/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jasuuk-state-file"
+    bucket = "jasland-state-file"
     region = "eu-west-1"
     key = "eks/terraform.tfstate"
     dynamodb_table = "jenkins-terraform-s3"
